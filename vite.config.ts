894956c1daec4ab97a599f9acef28a938cb09c85
Vimala2309 <144795201+Vimala2309@users.noreply.github.com>
2025-06-29 11:16:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/myportfolio/',
+  base:'/Myportfolio/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
